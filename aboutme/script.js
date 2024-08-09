343e5a4f06c1715fe2cdbdf8e4657eaea8afc6a6
Jonathan A <haljonathan21@gmail.com>
2024-08-09 16:41:33 -0400

    website done

---
+++ aboutme/script.js
@@ -0,0 +1,23 @@
+//This where the functionality begins
+
+function closeWindow(){
+    window.open('','_parent',''); 
+    window.close();
+}
+
+function openGitHub(){
+    window.open("https://github.com/joncineus"); 
+}
+
+function openLinkedIn(){
+    window.open("https://www.linkedin.com/in/jonathan-alcineus-78281b220/"); 
+}
+
+
+function openGmail(){
+    window.open("https://mail.google.com/mail/u/0/?tab=rm&ogbl#inbox?compose=GTvVlcRzCbRJkBBqKDpSWPzrHCGQKqGhLfJbJCFxHXfPVddhBhSlZGfRgjtnMpchGMkZbGfVXRTtC"); 
+}
+
+function openResume(){
+    window.open("https://docs.google.com/document/d/1CKOiZxGugqan7JAXto4wNiKeHPm8-3U5qDS20aAFXw8/edit?usp=sharing")
+}
